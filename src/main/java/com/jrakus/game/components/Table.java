5b506c957b67a11c1507ea57fe6462ee085c3090
kuba <jakub.rakus06@gmail.com>
2026-02-21 17:37:11 +0100

    add new validation for attacking cards

--- src/main/java/com/jrakus/game/components/Table.java
+++ src/main/java/com/jrakus/game/components/Table.java
@@ -7,12 +7,13 @@ public class Table {
     private final List<Card> defendingCards = new ArrayList<>();
 
     public void addAttackingCards(List<Card> newAttackingCards) {
+        checkNewAttackingCards(newAttackingCards);
         attackingCards.addAll(newAttackingCards);
     }
 
     public void addDefendingCards(List<Card> newDefendingCards, Card.Suit trump) {
 
-        checkDefendingCards(attackingCards, newDefendingCards, trump);
+        checkNewDefendingCards(attackingCards, newDefendingCards, trump);
         defendingCards.addAll(newDefendingCards);
     }
 
@@ -38,7 +39,10 @@ public class Table {
         return defendingCards;
     }
 
-    private void checkDefendingCards(List<Card> attackingCards, List<Card> defendingCards, Card.Suit trump) {
+    private void checkNewDefendingCards(List<Card> attackingCards, List<Card> defendingCards, Card.Suit trump) {
+
+        // TODO: fix this function, only attacking cards that are not defeated should be analyzed here
+
         if(attackingCards.size() != defendingCards.size())
             throw new RuntimeException("The amount of defending cards is different than attacking cards");
 
@@ -79,6 +83,38 @@ public class Table {
         }
 
         return rankValue1 - rankValue2;
+    }
+
+    private void checkNewAttackingCards(List<Card> newAttackingCards) {
+
+        int newNumberOfCardsOnTable = attackingCards.size() + newAttackingCards.size();
+
+        if (newNumberOfCardsOnTable > 5)
+            throw new RuntimeException("Exceeded limit of attacking cards on table.");
+
+        boolean areAnyAttackingCardsOnTable = !attackingCards.isEmpty();
+
+        if (areAnyAttackingCardsOnTable) {
+            checkIfNewAttackingCardsAreValid(newAttackingCards);
+        }
+    }
+
+    private void checkIfNewAttackingCardsAreValid(List<Card> newAttackingCards) {
+
+        Set<Card.Rank> allAttackingRanks = new HashSet<>();
+
+        attackingCards.forEach(attackingCard -> allAttackingRanks.add(attackingCard.rank()));
+        defendingCards.forEach(defendingCard -> allAttackingRanks.add(defendingCard.rank()));
+
+        for(Card newAttackingCard: newAttackingCards) {
+
+            Card.Rank rank = newAttackingCard.rank();
+
+            if(!allAttackingRanks.contains(rank))
+                throw new RuntimeException(
+                        String.format("Rank: %s has not been used before. You can't play it.", rank)
+                );
+        }
     }
 
     @Override
