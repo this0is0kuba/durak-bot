eaed0a94822aa5ad5996baf5aa602544069ddaff
kuba <jakub.rakus06@gmail.com>
2026-04-09 23:40:53 +0200

    add Tournament

--- src/main/java/com/jrakus/Main.java
+++ src/main/java/com/jrakus/Main.java
@@ -1,20 +1,21 @@
 package com.jrakus;
 
 import com.jrakus.players.Player;
+import com.jrakus.players.bots.RandomBot;
 import com.jrakus.players.bots.TrivialBot;
 import com.jrakus.players.bots.card_selector.CardSelector;
 import com.jrakus.playground.GameRoom;
+import com.jrakus.playground.Tournament;
 import com.jrakus.playground.displays.TerminalPrinter;
 
 public class Main {
     static void main() {
 
-        Player player1 = new TrivialBot(new CardSelector(), new TerminalPrinter());
-        Player player2 = new TrivialBot();
+        Player player1 = new TrivialBot();
+        Player player2 = new RandomBot();
 
-        GameRoom game = new GameRoom(player1, player2);
-
-        System.out.println("The game has started");
-        game.startGame();
+        Tournament tournament = new Tournament();
+        tournament.playTournament(player1, player2, 100);
+        tournament.displayStatistics();
     }
 }
